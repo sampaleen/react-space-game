fe3ad4523fa1663b0d93c669d7196da8e00508a6
Sampaleen <spaleen@udel.edu>
2017-09-18 17:48:05 -0400

    Up down left right controller

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Characters
+import Elf from './Characters/Elf';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Elf/>
           <h2>SpaceGAME xDDDDDDDD lol</h2>
-        </div>
       </div>
     );
   }
---
+++ src/Characters/Elf.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import keydown from 'react-keydown';
+import Mousetrap from 'mousetrap';
+
+class Elf extends Component {
+  constructor() {
+    super();
+    this.onKeyPress = this.onKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    Mousetrap.bind(['up','down','left','right'], this.onKeyPress);
+  }
+
+  onKeyPress(target) {
+    if(target.keyCode===38){
+      console.log('Up Clicked!!!');    
+    }
+    else if(target.keyCode===39){
+      console.log('Right Clicked!!!');    
+    }
+    else if(target.keyCode===40){
+      console.log('Down Clicked!!!');    
+    }
+    else if(target.keyCode===37){
+      console.log('Left Clicked!!!');    
+    }
+  }
+  
+  render() {
+    return(
+      <div />
+    );
+  }
+}
+
+export default Elf;
